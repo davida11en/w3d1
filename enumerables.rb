407db50caff3cefe7a5f0c11b64f57cd6163f4fa
Eric Balfour <egbalfour@gmail.com>
2022-04-11 16:33:25 -0400

    w3d1 EB edits

--- enumerables.rb
+++ enumerables.rb
@@ -21,5 +21,49 @@ class Array
         end
         newar
     end
+
+    def my_reject(&prc)
+        newar = []
+        i = 0
+        while i < self.length
+            if !prc.call(self[i])
+                newar << self[i]
+            end
+            i += 1
+        end
+        newar
+    end
+
+    def my_all?(&prc)
+        i = 0
+        while i < self.length
+            return false if !prc.call(self[i])
+            i += 1
+        end
+        true
+    end
+
+    def my_any?(&prc)
+        
+        i = 0
+        while i < self.length
+            return true if prc.call(self[i])
+            i += 1
+        end
+        false
+    end
+
+
+    def my_flatten
+        new_arr=[]
+        return[self] if !self.kind_of?(Array)
+        return self if self.none? {|ele| ele.kind_of?(Array)}
+        new_arr += self.flatten
+
+    end
+
+
+
+
 end
 
