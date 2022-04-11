cf962cee76ab491beb8d34cba870d8494e4d9a30
David-Allen <davida.asencio@gmail.com>
2022-04-11 17:48:47 -0400

    w3d1 edit

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Array
 
     def my_each(&prc)
@@ -59,11 +61,46 @@ class Array
         return[self] if !self.kind_of?(Array)
         return self if self.none? {|ele| ele.kind_of?(Array)}
         new_arr += self.flatten
-
     end
 
+    def my_zip(*args)
+        newar = Array.new(self.length) {Array.new}
+        joined = [self] + args
+        (0...self.length).each do |i|
+            joined.each do |el|
+                newar[i] << el[i]
+            end
+        end
+        newar
+    end
 
+    def my_rotate(n = 1)
+        if n > 0
+            n.times do 
+                first = self.push(self.first)
+                self.shift
+            end
+        else 
+            n.abs.times do 
+                last = self.pop()
+                self.unshift(last)
+            end
+        end
+        self
+    end
 
-
+    def join(sep = '')
+        str = ''
+        i = 0
+        while i < self.length
+            if i != self.length
+                str += self[i] + sep
+            elsif i == self.length - 1
+                str += self[i]
+            end
+            i += 1
+        end 
+        str
+    end
 end
 
